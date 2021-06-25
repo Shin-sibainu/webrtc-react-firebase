f8478b9e7c3aa89b4c5dc5752e5c631716c5e88e
Shintakumi <shincode0712@gmail.com>
2021-06-25 20:53:55 +0900

    install material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const getMedia = async () => {
   const constraints = {audio: true, video: true };
 
   try {
+    //getUserMediaのプロミス(resolve, reject)が返ってくるまで待機する。
     return await navigator.mediaDevices.getUserMedia(constraints);
     /* ストリームを使用 */
   } catch(err) {
@@ -14,7 +16,7 @@ const getMedia = async () => {
 getMedia();
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return <Button color='primary' variant='contained'>Hello, World!</Button>;
 }
 
 export default App;
